4e4cc9c465aeadcfbfd0f035112c4809f8c3d767
youngmin30 <jjymjj@naver.com>
2023-03-05 20:04:13 +0900

    23020305 이자다 chap01

--- src/chap01/Hello.java
+++ src/chap01/Hello.java
@@ -1,8 +1,7 @@
-package chap01;
+﻿package chap01;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, welcome to the java world!");
-    }
+	public static void main(String[] args) { // 실행 클래스 main() 메소드
+		System.out.println("Hello, welcome to the java world!"); // println() 메소드, 줄바꿈 포함한 출력
+	}
 }
-
