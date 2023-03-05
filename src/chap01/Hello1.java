18302dd89a198b4cda5aae8b09d8e296196d6206
youngmin30 <jjymjj@naver.com>
2023-03-05 20:06:44 +0900

    Hello

--- src/chap01/Hello1.java
+++
@@ -1,7 +0,0 @@
-﻿package chap01;
-
-public class Hello1 {
-	public static void main(String[] args) { // 실행 클래스 main() 메소드
-		System.out.println("Hello, welcome to the java world!"); // println() 메소드, 줄바꿈 포함한 출력
-	}
-}
